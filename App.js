7ffd1286b77c7705ba45b076d1e25cd163741eab
Baptiste Lacoste <baptiste@MacBook-Air-de-Baptiste.local>
2022-12-12 14:25:49 +0100

    debug app.js avec import

--- App.js
+++ App.js
@@ -1,29 +1,22 @@
-import { StyleSheet, Text, View } from "react-native"
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import FontAwesome from "react-native-vector-icons/FontAwesome";
+
+import Home from "./screens/Home";
+import Login from "./screens/Login";
+
+const Stack = createNativeStackNavigator();
+const Tab = createBottomTabNavigator();
 
 export default function App() {
-	return (
-		<Provider store={store}>
-			<NavigationContainer>
-				<Stack.Navigator screenOptions={{ headerShown: true }}>
-					<Stack.Screen
-						name="Home"
-						component={HomeScreen}
-					/>
-					<Stack.Screen
-						name="Login"
-						component={Login}
-					/>
-				</Stack.Navigator>
-			</NavigationContainer>
-		</Provider>
-	)
+  return (
+    <NavigationContainer>
+      <Stack.Navigator screenOptions={{ headerShown: false }}>
+        <Stack.Screen name="Home" component={Home} />
+        <Stack.Screen name="Login" component={Login} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
--- screens/Home.js
+++ screens/Home.js
@@ -1,17 +1,12 @@
-import { Image, StyleSheet } from "react-native"
+import { Image, StyleSheet } from "react-native";
 
 export default function MapScreen() {
-	return (
-		<Image
-			source={require("../assets/adaptive-icon.png")}
-			style={styles.map}
-		/>
-	)
+  return <Image style={styles.map} />;
 }
 
 const styles = StyleSheet.create({
-	map: {
-		width: "100%",
-		height: "100%",
-	},
-})
+  map: {
+    width: "100%",
+    height: "100%",
+  },
+});
