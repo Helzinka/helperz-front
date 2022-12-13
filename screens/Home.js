2dd74c5729ced815f0ec8bc2926dd7a96c830b1e
Jimmy <jim.b3860@gmail.com>
2022-12-13 09:34:21 +0100

    firts commit création Announce

--- screens/Home.js
+++ screens/Home.js
@@ -1,12 +1,12 @@
 import { StyleSheet, Text } from "react-native";
 
 export default function MapScreen() {
-  return <Text> </Text>;
+	return <Text>bonjour</Text>;
 }
 
 const styles = StyleSheet.create({
-  map: {
-    width: "100%",
-    height: "100%",
-  },
+	map: {
+		width: "100%",
+		height: "100%",
+	},
 });
