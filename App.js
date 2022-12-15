71a2c086a642fec1e93c9ef4e415a37690f61992
BaptisteLac <baptiste.lacoste0@gmail.com>
2022-12-15 09:13:32 +0100

    set up menu

--- App.js
+++ App.js
@@ -3,6 +3,10 @@ import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { createDrawerNavigator } from "@react-navigation/drawer";
 
+import { StyleSheet } from "react-native";
+
+import FontAwesome from "react-native-vector-icons/FontAwesome";
+
 import Home from "./screens/Home";
 import Login from "./screens/Login";
 import AnnonceRecap from "./screens/AnnonceRecap";
@@ -20,39 +24,42 @@ const Drawer = createDrawerNavigator();
 import user from "./reducers/user";
 
 const store = configureStore({
-	reducer: { user },
+  reducer: { user },
 });
 ("trou de balle");
 
 const DrawerNavigator = () => {
-	return (
-		<Drawer.Navigator // Fonction pour le menu
-			initialRouteName="Home"
-			screenOptions={{
-				header: (props) => <Header {...props} />,
-				drawerActiveTintColor: "#00C6A0",
-				drawerType: "back",
-				drawerPosition: "right", // Positionne le menu à droite
-			}}
-		>
-			<Drawer.Screen name="Accueil" component={Home} />
-			<Drawer.Screen name="Créer une annonce" component={CreateAnnounce} />
-			<Drawer.Screen name="Profile" component={Profil} />
-			<Drawer.Screen name="Se connecter" component={Login} />
-			<Drawer.Screen name="Annnonces validées" component={AnnonceRecap} />
-			<Drawer.Screen name="Mes annonces" component={PageAnnonce} />
-		</Drawer.Navigator>
-	);
+  return (
+    <Drawer.Navigator // Fonction pour le menu
+      initialRouteName="Home"
+      screenOptions={{
+        header: (props) => <Header {...props} />,
+        drawerActiveTintColor: "#00C6A0",
+        drawerType: "back",
+        drawerPosition: "right", // Positionne le menu à droite
+      }}
+      style={styles.drawer}
+    >
+      <Drawer.Screen name="Accueil" component={Home} />
+      <Drawer.Screen name="Créer une annonce" component={CreateAnnounce} />
+      <Drawer.Screen name="Profile" component={Profil} />
+      <Drawer.Screen name="Se connecter" component={Login} />
+      <Drawer.Screen name="Annnonces validées" component={AnnonceRecap} />
+      <Drawer.Screen name="Mes annonces" component={PageAnnonce} />
+    </Drawer.Navigator>
+  );
 };
 
 export default function App() {
-	return (
-		<Provider store={store}>
-			<NavigationContainer>
-				<Stack.Navigator screenOptions={{ headerShown: false }}>
-					<Stack.Screen name="DrawerNavigator" component={DrawerNavigator} />
-				</Stack.Navigator>
-			</NavigationContainer>
-		</Provider>
-	);
+  return (
+    <Provider store={store}>
+      <NavigationContainer>
+        <Stack.Navigator screenOptions={{ headerShown: false }}>
+          <Stack.Screen name="DrawerNavigator" component={DrawerNavigator} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </Provider>
+  );
 }
+
+const styles = StyleSheet.create({});
