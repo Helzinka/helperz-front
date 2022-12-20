27d049eef74cba0df643897f85f8be16a605e225
Helzinka <leveque.fra@gmail.com>
2022-12-20 09:44:52 +0100

    login

--- screens/Home.js
+++ screens/Home.js
@@ -1,138 +1,139 @@
-import {
-  SafeAreaView,
-  StyleSheet,
-  Image,
-  View,
-  TouchableOpacity,
-  Text,
-} from "react-native";
-import { useSelector } from "react-redux";
+import { SafeAreaView, StyleSheet, Image, View, TouchableOpacity, Text } from "react-native"
+import { useSelector } from "react-redux"
 
 export default function HomeScreen({ navigation }) {
-  return (
-    <SafeAreaView style={styles.container}>
-      <Image
-        source={require("../assets/Carrousel.jpg")}
-        style={styles.carrousel}
-      />
-      <View style={styles.content}>
-        <TouchableOpacity
-          style={styles.card}
-          activeOpacity={0.8}
-          onPress={() => navigation.navigate("Créer une annonce")}
-        >
-          {/* <FontAwesomeIcon
+	const userReducer = useSelector((state) => state.user.value)
+	const handleLogin = () => {
+		if (userReducer.user.token) {
+			navigation.navigate("Créer une annonce")
+		} else {
+			navigation.navigate("Se connecter")
+		}
+	}
+	return (
+		<SafeAreaView style={styles.container}>
+			<Image
+				source={require("../assets/Carrousel.jpg")}
+				style={styles.carrousel}
+			/>
+			<View style={styles.content}>
+				<TouchableOpacity
+					style={styles.card}
+					activeOpacity={0.8}
+					onPress={() => handleLogin()}
+				>
+					{/* <FontAwesomeIcon
             icon={faShop}
             size={50}
             color={"red"}
           ></FontAwesomeIcon> */}
-          <Image
-            source={require("../assets/annonce.png")}
-            style={styles.imageCard}
-          />
-          <View style={styles.textCard}>
-            <Text style={styles.titreCard}>Créez une annonce</Text>
-            <Text style={styles.textContent}>
-              Trouvez un.e Helperz qui vous accompagnera lors de votre achat
-            </Text>
-          </View>
-        </TouchableOpacity>
+					<Image
+						source={require("../assets/annonce.png")}
+						style={styles.imageCard}
+					/>
+					<View style={styles.textCard}>
+						<Text style={styles.titreCard}>Créez une annonce</Text>
+						<Text style={styles.textContent}>
+							Trouvez un.e Helperz qui vous accompagnera lors de votre achat
+						</Text>
+					</View>
+				</TouchableOpacity>
 
-        <TouchableOpacity
-          style={styles.card}
-          activeOpacity={0.8}
-          onPress={() => navigation.navigate("Liste helperz")}
-        >
-          <Image
-            source={require("../assets/trouver.png")}
-            style={styles.imageCard}
-          />
-          <View style={styles.textCard}>
-            <Text style={styles.titreCard}>Trouvez un Helperz</Text>
-            <Text style={styles.textContent}>
-              Nous vous mettons en relation avec des experts pour vos achats
-            </Text>
-          </View>
-        </TouchableOpacity>
+				<TouchableOpacity
+					style={styles.card}
+					activeOpacity={0.8}
+					onPress={() => navigation.navigate("Liste helperz")}
+				>
+					<Image
+						source={require("../assets/trouver.png")}
+						style={styles.imageCard}
+					/>
+					<View style={styles.textCard}>
+						<Text style={styles.titreCard}>Trouvez un Helperz</Text>
+						<Text style={styles.textContent}>
+							Nous vous mettons en relation avec des experts pour vos achats
+						</Text>
+					</View>
+				</TouchableOpacity>
 
-        <TouchableOpacity
-          style={styles.card}
-          activeOpacity={0.8}
-          onPress={() => navigation.navigate("Liste annonces")}
-        >
-          <Image
-            source={require("../assets/money.png")}
-            style={styles.imageCard}
-          />
-          <View style={styles.textCard}>
-            <Text style={styles.titreCard}>Faites vous rémunérer</Text>
-            <Text style={styles.textContent}>
-              Gagnez de l'argent dès que vous réalisez une mission
-            </Text>
-          </View>
-        </TouchableOpacity>
-      </View>
-    </SafeAreaView>
-  );
+				<TouchableOpacity
+					style={styles.card}
+					activeOpacity={0.8}
+					onPress={() => navigation.navigate("Liste annonces")}
+				>
+					<Image
+						source={require("../assets/money.png")}
+						style={styles.imageCard}
+					/>
+					<View style={styles.textCard}>
+						<Text style={styles.titreCard}>Faites vous rémunérer</Text>
+						<Text style={styles.textContent}>
+							Gagnez de l'argent dès que vous réalisez une mission
+						</Text>
+					</View>
+				</TouchableOpacity>
+			</View>
+		</SafeAreaView>
+	)
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "white",
-  },
-  carrousel: {
-    flex: 1.9,
-    width: "100%",
-    height: "50%",
-  },
-  content: {
-    flex: 3,
-    flexDirection: "column",
-    alignItems: "center",
-    justifyContent: "space-evenly",
-  },
-  card: {
-    flexDirection: "row",
-    width: "90%",
-    height: "28%",
-    alignItems: "center",
-    justifyContent: "space-between",
-    shadowColor: "#000000",
-    shadowOffset: {
-      width: 0,
-      height: 4,
-    },
-    shadowOpacity: 0.19,
-    shadowRadius: 5.62,
-    elevation: 6,
-  },
-  imageCard: {
-    width: "30%",
-    height: "100%",
-    borderBottomLeftRadius: 20,
-    borderTopLeftRadius: 20,
-  },
-  textCard: {
-    backgroundColor: "#00C6A0",
-    borderTopRightRadius: 20,
-    borderBottomRightRadius: 20,
-    // borderWidth: 2,
-    // borderColor: "#00C6A0",
-    width: "68%",
-    height: "100%",
-    flexDirection: "column",
-    justifyContent: "space-evenly",
-    alignItems: "center",
-    padding: 5,
-  },
-  titreCard: {
-    fontWeight: "bold",
-    fontSize: 22,
-    color: "white",
-  },
-  textContent: {
-    fontSize: 14,
-    color: "white",
-  },
-});
+	container: {
+		flex: 1,
+		backgroundColor: "white",
+	},
+	carrousel: {
+		flex: 1.9,
+		width: "100%",
+		height: "50%",
+	},
+	content: {
+		flex: 3,
+		flexDirection: "column",
+		alignItems: "center",
+		justifyContent: "space-evenly",
+	},
+	card: {
+		flexDirection: "row",
+		width: "90%",
+		height: "28%",
+		alignItems: "center",
+		justifyContent: "space-between",
+		shadowColor: "#000000",
+		shadowOffset: {
+			width: 0,
+			height: 4,
+		},
+		shadowOpacity: 0.19,
+		shadowRadius: 5.62,
+		elevation: 6,
+	},
+	imageCard: {
+		width: "30%",
+		height: "100%",
+		borderBottomLeftRadius: 20,
+		borderTopLeftRadius: 20,
+	},
+	textCard: {
+		backgroundColor: "#00C6A0",
+		borderTopRightRadius: 20,
+		borderBottomRightRadius: 20,
+		// borderWidth: 2,
+		// borderColor: "#00C6A0",
+		width: "68%",
+		height: "100%",
+		flexDirection: "column",
+		justifyContent: "space-evenly",
+		alignItems: "center",
+		padding: 5,
+	},
+	titreCard: {
+		fontWeight: "bold",
+		fontSize: 22,
+		color: "white",
+	},
+	textContent: {
+		fontSize: 14,
+		color: "white",
+	},
+})
--- screens/ListHelperz.js
+++ screens/ListHelperz.js
@@ -1,178 +1,181 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState } from "react"
 import {
-  View,
-  Text,
-  TextInput,
-  StyleSheet,
-  ScrollView,
-  Pressable,
-  TouchableOpacity,
-} from "react-native";
-import FontAwesome from "react-native-vector-icons/FontAwesome";
-import Card from "../components/Card";
-import FilterModal from "../modals/Filter";
-import mongodb from "../data.json";
+	View,
+	Text,
+	TextInput,
+	StyleSheet,
+	ScrollView,
+	Pressable,
+	TouchableOpacity,
+} from "react-native"
+import FontAwesome from "react-native-vector-icons/FontAwesome"
+import Card from "../components/Card"
+import FilterModal from "../modals/Filter"
+import mongodb from "../data.json"
 // fake data mongodb
-import { IP_LOCAL } from "@env";
-import MapView, { Marker } from "react-native-maps";
-import { useSelector } from "react-redux";
+import { IP_LOCAL } from "@env"
+import MapView, { Marker } from "react-native-maps"
+import { useSelector } from "react-redux"
 
 export default function Annonce({ navigation }) {
-  const BASE_URL = `http://${IP_LOCAL}:3000`;
-  const [data, setData] = useState();
-  const [initLocation, setInitLocation] = useState({});
-  const [search, setSearch] = useState();
-  const [isModalVisible, setIsModalVisible] = useState(false);
-  const UserReducer = useSelector((state) => state.user.value);
+	const BASE_URL = `http://${IP_LOCAL}:3000`
+	const [data, setData] = useState()
+	const [initLocation, setInitLocation] = useState({})
+	const [search, setSearch] = useState()
+	const [isModalVisible, setIsModalVisible] = useState(false)
+	const UserReducer = useSelector((state) => state.user.value)
 
-  // affiche la modal au click de l'icon "+"
-  const isVisible = () => {
-    setIsModalVisible(!isModalVisible);
-  };
+	// affiche la modal au click de l'icon "+"
+	const isVisible = () => {
+		setIsModalVisible(!isModalVisible)
+	}
 
-  // ferme la modal au click dans la modal "quitter"
-  const onClose = () => {
-    setIsModalVisible(!isModalVisible);
-  };
-  // recupère les données à la création du composant depuis data.json
-  useEffect(() => {
-    // on récupère la première annnonce depuusi le reducer user
-    let last = UserReducer.announces.length - 1;
-    const [lat, long] = [
-      UserReducer.announces[last].location.lat,
-      UserReducer.announces[last].location.long,
-    ];
-    setInitLocation({ lat: lat, long: long });
-    const announceLocation = UserReducer.announces[last].location.name;
-    // on précise le lieu de l'annonce et on fetch tout les helperz sur la meme ville
-    fetch(`${BASE_URL}/users/helperz/${announceLocation}`)
-      .then((response) => response.json())
-      .then((data) => {
-        setData(data);
-      });
-  }, []);
+	// ferme la modal au click dans la modal "quitter"
+	const onClose = () => {
+		setIsModalVisible(!isModalVisible)
+	}
+	// recupère les données à la création du composant depuis data.json
+	useEffect(() => {
+		// on récupère la première annnonce depuis le reducer user
+		let last = UserReducer.announces.length - 1
+		const [lat, long] = [
+			UserReducer.announces[last].location.lat,
+			UserReducer.announces[last].location.long,
+		]
+		setInitLocation({ lat: lat, long: long })
+		const announceLocation = UserReducer.announces[last].location.name
+		// on précise le lieu de l'annonce et on fetch tout les helperz sur la meme ville
+		fetch(`${BASE_URL}/users/helperz/${announceLocation}`)
+			.then((response) => response.json())
+			.then((data) => {
+				setData(data)
+			})
+	}, [])
 
-  // fonction pour retourner toutes les helperz via le composant "Card"
-  // check si les données sont présente via if(data) !important
-  const showAnnounce = () => {
-    if (data) {
-      return data.user.map((value, index) => (
-        <Card key={index} data={value} type="helperz" />
-      ));
-    }
-  };
-  const showmarker = () => {
-    if (data) {
-      return data.user.map((value, index) => {
-        const [lat, long] = [
-          value.helperz.location.lat,
-          value.helperz.location.long,
-        ];
-        console.log(lat, long);
-        return (
-          <Marker
-            key={index}
-            pinColor="red"
-            coordinate={{ latitude: lat, longitude: long }}
-            title={value.username}
-          ></Marker>
-        );
-      });
-    }
-  };
+	// fonction pour retourner toutes les helperz via le composant "Card"
+	// check si les données sont présente via if(data) !important
+	const showAnnounce = () => {
+		if (data) {
+			return data.user.map((value, index) => (
+				<Card
+					key={index}
+					data={value}
+					type="helperz"
+				/>
+			))
+		}
+	}
+	const showmarker = () => {
+		if (data) {
+			return data.user.map((value, index) => {
+				const [lat, long] = [value.helperz.location.lat, value.helperz.location.long]
+				return (
+					<Marker
+						key={index}
+						pinColor="red"
+						coordinate={{ latitude: lat, longitude: long }}
+						title={value.username}
+					></Marker>
+				)
+			})
+		}
+	}
 
-  return (
-    <>
-      <View style={styles.container}>
-        <TextInput
-          style={styles.search}
-          onChangeText={setSearch}
-          value={search}
-          placeholder="Location"
-        ></TextInput>
-        <View style={styles.filters}>
-          <TouchableOpacity onPress={() => isVisible()}>
-            <FontAwesome
-              name="plus"
-              size={20}
-              style={styles.plus}
-            ></FontAwesome>
-          </TouchableOpacity>
-          <ScrollView horizontal={true} showsHorizontalScrollIndicator={false}>
-            <Pressable style={styles.filter}>
-              <Text>Paris</Text>
-            </Pressable>
-            <Pressable style={styles.filter}>
-              <Text>Date</Text>
-            </Pressable>
-          </ScrollView>
-        </View>
-      </View>
-      {initLocation && (
-        <MapView
-          region={{
-            latitude: initLocation.lat,
-            longitude: initLocation.long,
-            latitudeDelta: 0.0922,
-            longitudeDelta: 0.0421,
-          }}
-          style={styles.mapView}
-        >
-          {showmarker()}
-        </MapView>
-      )}
-      <View style={styles.announces}>{showAnnounce()}</View>
-      <FilterModal
-        isVisible={isModalVisible}
-        onClose={() => onClose()}
-      ></FilterModal>
-    </>
-  );
+	return (
+		<>
+			<View style={styles.container}>
+				<TextInput
+					style={styles.search}
+					onChangeText={setSearch}
+					value={search}
+					placeholder="Location"
+				></TextInput>
+				<View style={styles.filters}>
+					<TouchableOpacity onPress={() => isVisible()}>
+						<FontAwesome
+							name="plus"
+							size={20}
+							style={styles.plus}
+						></FontAwesome>
+					</TouchableOpacity>
+					<ScrollView
+						horizontal={true}
+						showsHorizontalScrollIndicator={false}
+					>
+						<Pressable style={styles.filter}>
+							<Text>Paris</Text>
+						</Pressable>
+						<Pressable style={styles.filter}>
+							<Text>Date</Text>
+						</Pressable>
+					</ScrollView>
+				</View>
+			</View>
+			{initLocation && (
+				<MapView
+					region={{
+						latitude: initLocation.lat,
+						longitude: initLocation.long,
+						latitudeDelta: 0.0922,
+						longitudeDelta: 0.0421,
+					}}
+					style={styles.mapView}
+				>
+					{showmarker()}
+				</MapView>
+			)}
+			<View style={styles.announces}>{showAnnounce()}</View>
+			<FilterModal
+				isVisible={isModalVisible}
+				onClose={() => onClose()}
+			></FilterModal>
+		</>
+	)
 }
 
 const styles = StyleSheet.create({
-  container: {
-    backgroundColor: "white",
-  },
-  search: {
-    alignSelf: "center",
-    width: "85%",
-    padding: 10,
-    margin: 10,
-    borderRadius: 10,
-    backgroundColor: "white",
-    borderRadius: 10,
-    borderColor: "#00C6A0",
-    borderWidth: 3,
-  },
-  filters: {
-    margin: 15,
-    flexDirection: "row",
-    justifyContent: "space-between",
-    alignItems: "center",
-    backgroundColor: "white",
-  },
-  filter: {
-    backgroundColor: "#00C6A0",
-    borderRadius: 8,
-    paddingRight: 8,
-    paddingLeft: 8,
-    paddingTop: 4,
-    paddingBottom: 4,
-    marginRight: 8,
-  },
-  plus: {
-    marginLeft: 10,
-    marginRight: 20,
-  },
-  announces: {
-    flex: 1,
-    alignItems: "center",
-    backgroundColor: "white",
-    marginTop: 20,
-  },
-  mapView: {
-    flex: 2,
-    height: "100%",
-  },
-});
+	container: {
+		backgroundColor: "white",
+	},
+	search: {
+		alignSelf: "center",
+		width: "85%",
+		padding: 10,
+		margin: 10,
+		borderRadius: 10,
+		backgroundColor: "white",
+		borderRadius: 10,
+		borderColor: "#00C6A0",
+		borderWidth: 3,
+	},
+	filters: {
+		margin: 15,
+		flexDirection: "row",
+		justifyContent: "space-between",
+		alignItems: "center",
+		backgroundColor: "white",
+	},
+	filter: {
+		backgroundColor: "#00C6A0",
+		borderRadius: 8,
+		paddingRight: 8,
+		paddingLeft: 8,
+		paddingTop: 4,
+		paddingBottom: 4,
+		marginRight: 8,
+	},
+	plus: {
+		marginLeft: 10,
+		marginRight: 20,
+	},
+	announces: {
+		flex: 1,
+		alignItems: "center",
+		backgroundColor: "white",
+		marginTop: 20,
+	},
+	mapView: {
+		flex: 2,
+		height: "100%",
+	},
+})
